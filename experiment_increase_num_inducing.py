6d6996a239689899b7a0c7ee665addb3f7ddf952
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-02-28 11:51:28 +0000

    Use tqdm to monitor progress for multiple num_inducing experiment

--- experiment_increase_num_inducing.py
+++ experiment_increase_num_inducing.py
@@ -3,19 +3,22 @@ import matplotlib.pyplot as plt
 from main import create_full_gp, create_sparse_gp
 from compare import KL_divergence, diff_marginal_likelihoods
 from simulation import simulate_data
+import tqdm
 
 USE_MARGINAL_LIKELIHOOD_DIFF = True
 
 
 def main():
     """Run experiment with changing number of inducing variables."""
-    X, y = simulate_data(50, 1)
+    data = simulate_data(500, 1)
+    X, y = data.X_train, data.y_train
     m_full = create_full_gp(X, y, plot=False)
     results = []
     n = len(X)
     # Increase the number of inducing inputs until n==m
     inducing_inputs_nums = range(1, n + 1)
-    for num in inducing_inputs_nums:
+    for i in tqdm.trange(len(inducing_inputs_nums)):
+        num = inducing_inputs_nums[i]
         m_sparse = create_sparse_gp(X, y, num_inducing=num, plot=False)
         if USE_MARGINAL_LIKELIHOOD_DIFF:
             divergence = diff_marginal_likelihoods(m_sparse, m_full, True)
